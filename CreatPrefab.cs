ee1824ab27faf6026f585cde5c3fac5fb145506c
大槻亮太 <ootsukiryouta@MacBook-Pro-13-inch-2018.local>
2020-03-11 08:24:59 +0900

    ファイルを追加

---
+++ CreatPrefab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatPrefab : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            var go = Instantiate(prefab) as GameObject;
+
+            go.transform.position = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ MovedObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovedObject : MonoBehaviour
+{
+    private Vector3 screenPoint, offset;
+
+    [SerializeField] GameObject me;
+
+    void OnMouseDown()
+    {
+        // このオブジェクトの位置(transform.position)をスクリーン座標に変換。
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        // ワールド座標上の、マウスカーソルと、対象の位置の差分。
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + offset;
+        transform.position = currentPosition;
+    }
+
+    void Update()
+    {
+        //
+        if (Input.GetMouseButtonUp(1))
+        {
+            me.GetComponent<Rigidbody>().isKinematic = false;
+
+            me.GetComponent<MovedObject>().enabled = false;
+        }
+    }
+}
